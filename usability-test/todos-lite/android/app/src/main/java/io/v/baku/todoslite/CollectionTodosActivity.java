83139d274787734bdefef62ded9735fbcf6c2d87
Ross Wang <rosswang@google.com>
2016-02-25 14:20:35 -0800

    Tweaking TODOs
    Mention Syncbase explicitly while encouraging more open-ended solutions
    by not leading towards bindReadOnly + put.
    
    Change-Id: I7d464ff32f1dd084be79739934340cd08bc8fc0e

--- usability-test/todos-lite/android/app/src/main/java/io/v/baku/todoslite/CollectionTodosActivity.java
+++ usability-test/todos-lite/android/app/src/main/java/io/v/baku/todoslite/CollectionTodosActivity.java
@@ -14,14 +14,13 @@ public class CollectionTodosActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todos_collection);
 
-        // TODO: Add logic to bind a data prefix to the todoItems ListView.
+        // TODO: Add logic to bind a Syncbase data prefix to the todoItems ListView.
 
         findViewById(R.id.add).setOnClickListener(bn -> {
             final EditText txtNewItem = (EditText) findViewById(R.id.newItemText);
             final String newItem = txtNewItem.getText().toString();
 
-            // TODO: Add logic to add the text from newItemText as a data row with the above prefix
-            // when the add button is clicked.
+            // TODO: Add logic to add the text from newItemText when the add button is clicked.
 
             txtNewItem.setText("");
         });
--- usability-test/todos-lite/android/app/src/main/java/io/v/baku/todoslite/ScalarTodosActivity.java
+++ usability-test/todos-lite/android/app/src/main/java/io/v/baku/todoslite/ScalarTodosActivity.java
@@ -14,14 +14,13 @@ public class ScalarTodosActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todos_scalar);
 
-        // TODO: Add logic to bind data to the todoItems TextView.
+        // TODO: Add logic to bind Syncbase data to the todoItems TextView.
 
         findViewById(R.id.add).setOnClickListener(bn -> {
             final EditText txtNewItem = (EditText) findViewById(R.id.newItemText);
             final String newItem = txtNewItem.getText().toString();
 
-            // TODO: Add logic to add the text from newItemText to the data field when the add
-            // button is clicked.
+            // TODO: Add logic to add the text from newItemText when the add button is clicked.
 
             txtNewItem.setText("");
         });
